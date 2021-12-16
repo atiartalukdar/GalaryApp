40a1f0599d60c88e93615d9e6032387f29a86ea6
Atiar Talukdar <atiartalukdar@gmail.com>
2021-12-17 00:45:18 +0900

    Initial galary view created.

---
+++ app/src/main/java/info/atiar/galaryapp/adapter/AlphabetAdapter.kt
@@ -0,0 +1,46 @@
+package info.atiar.galaryapp.adapter
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import info.atiar.galaryapp.R
+import info.atiar.galaryapp.model.CharItem
+
+class AlphabetAdapter(var context: Context, var arrayList: ArrayList<CharItem>) :
+        RecyclerView.Adapter<AlphabetAdapter.ItemHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+            val viewHolder = LayoutInflater.from(parent.context)
+                .inflate(R.layout.layout_grid_view_items, parent, false)
+            return ItemHolder(viewHolder)
+        }
+
+        override fun getItemCount(): Int {
+            return arrayList.size
+        }
+
+        override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+
+            val charItem: CharItem = arrayList.get(position)
+
+            holder.icons.setImageResource(charItem.icons!!)
+            holder.titles.text = charItem.alpha
+
+            holder.titles.setOnClickListener {
+                Toast.makeText(context, charItem.alpha, Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+        class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+            var icons = itemView.findViewById<ImageView>(R.id.icon_image_view)
+            var titles = itemView.findViewById<TextView>(R.id.title_text_view)
+
+        }
+
+}
